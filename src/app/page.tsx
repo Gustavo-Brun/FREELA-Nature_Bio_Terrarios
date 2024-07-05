b9e4d5ab8e4cf3d7e68175a67d7ea68c23102235
Gustavo Brun <gustavobdossantos.gb@gmail.com>
2024-07-04 21:05:45 -0300

    style: format the file structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { BudgetRequest } from "@/components/BudgetRequest";
-import { Contact } from "@/components/Contact";
-import { Feedback } from "@/components/Feedback";
 import { Header } from "@/components/Header";
 import { Products } from "@/components/Products";
+import { Feedback } from "@/components/Feedback";
+import { BudgetRequest } from "@/components/BudgetRequest";
+import { Contact } from "@/components/Contact";
 
 export default function Home(): JSX.Element {
   return (
